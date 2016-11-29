4494b8aa252e7e4bca6726565f63763a27508b96
liubin <liubindobest@gmail.com>
2016-11-29 23:29:10 +0900

    UI
    111

--- app/src/main/java/com/happypiebinliu/samplelearning/EditTextActivity.java
+++ app/src/main/java/com/happypiebinliu/samplelearning/EditTextActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
  * 练习了表格布局方式，可输入框里面，进行了稍微的用户体验扩充
  * 1 Hint 自动提示要输入的内容
  * 2 自动切换键盘 数字键盘和英文键盘
+ * 他继承了TextView 实际是就是一个可以编辑的TextView
  */
 public class EditTextActivity extends AppCompatActivity {
 
