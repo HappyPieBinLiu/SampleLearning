e57c543da4e266b3bdba057f11bff3c57ee23d64
HappyPieBinLiu <l58821263899b>
2016-12-22 18:03:28 +0900

    123

---
+++ app/src/main/java/com/happypiebinliu/samplelearning/ActivityImage/DrawTextOnPathActivity.java
@@ -0,0 +1,12 @@
+package com.happypiebinliu.samplelearning.ActivityImage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawTextOnPathActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/happypiebinliu/samplelearning/View/myView.java
@@ -0,0 +1,10 @@
+package com.happypiebinliu.samplelearning.View;
+
+import android.view.View;
+
+/**
+ * Created by ope001 on 2016/12/21.
+ */
+
+public class myView extends View {
+}
